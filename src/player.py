7f51ccb026fcb22c6623942fa4d4e9894ca0ed09
TrueBabyChaise <tomaugier23@gmail.com>
2021-11-12 23:02:09 +0100

    Add Complete visual and player input

--- src/player.py
+++ src/player.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 
 import sys
+import src.color as clr
 
 class Player():
+
 	def __init__(self, discStyle) -> None:
 		self.__discStyle = discStyle
+		self.__color = None
 		self.__name = ""
 
 	def __repr__(self) -> str:
-		return "Player : " + self.__name + ", Disc : " + self.__discStyle
+		return f"Player : {self.__name}, Disc : {self.__discStyle}, Color : {self.__color}Color{clr.Color_Off}"
 
 	def __str__(self) -> str:
-		return "Player : " + self.__name + ", Disc : " + self.__discStyle
+		return f"Player : {self.__name}, Disc : {self.__discStyle}, Color : {self.__color}Color{clr.Color_Off}"
 
 	@property
 	def name(self) -> str:
@@ -21,9 +24,31 @@ class Player():
 	def discStyle(self) -> str:
 		return self.__discStyle
 
+	@property
+	def color(self) -> str:
+		return self.__color
+	
 	@name.setter
 	def name(self, value):
 		self.__name = value
+
+	def askName(self) -> str:
+		tmp = "a"
+		accept = "a"		
+		while accept != "yes" and accept != "y":
+			tmp = input("Which name do you want to use ?\n -> ")
+			accept = input("Are you sure (y/n) ?\n -> ").lower()
+		self.__name = tmp
+		return tmp
+
+	def askColor(self) -> str:
+		colorTabValue = [clr.Red, clr.Black, clr.Blue, clr.White, clr.Yellow]
+		colorTab = ["red", "black","blue", "white", "yellow"]
+		tmp = "a"
+		while not tmp in colorTab:
+			tmp = input("Which color do you wanna play with " + self.__name + " ?\n[red, black, blue, white, yellow]\n -> ")
+		self.__color = colorTabValue[colorTab.index(tmp)]
+		return colorTabValue[colorTab.index(tmp)]
 
 	def askColumn(self) -> int:
 		tmp = "a"
--- src/table.py
+++ src/table.py
@@ -2,6 +2,7 @@
 
 from src.player import Player
 from src.connectFour import ConnectFour
+from src.color import Color_Off
 
 class Table():
 
@@ -15,31 +16,76 @@ class Table():
 		self.boardInit()
 		self.playerInit()
 
-	def boardInit(self):
+	def __repr__(self) -> str:
+		return "Table you play on :3"
+
+	def __str__(self) -> str:
+		string = ""
+		tmpTab = [[" " for i in range(self.__nbColumn * 4)] for y in range(self.__nbRow * 4)]
+		for row in range(len(self.__connectFour._board)):
+			for column in range(len(self.__connectFour._board[row])):
+				### TMP
+				tmpTab[row * 4][column  * 4] = "#"
+				tmpTab[row * 4][column * 4 + 1] = "#"
+				tmpTab[row * 4][column * 4 + 2] = "#"
+				tmpTab[row * 4][column * 4 + 3] = "#"
+				tmpTab[row * 4 + 1][column * 4] = "#"
+				tmpTab[row * 4 + 1][column * 4 + 1] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 1][column * 4 + 2] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 1][column * 4 + 3] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 2][column * 4] = "#"
+				tmpTab[row * 4 + 2][column * 4 + 1] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 2][column * 4 + 2] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 2][column * 4 + 3] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 3][column * 4] = "#"
+				tmpTab[row * 4 + 3][column * 4 + 1] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 3][column * 4 + 2] = self.getCaseFormat(self.__connectFour._board[row][column])
+				tmpTab[row * 4 + 3][column * 4 + 3] = self.getCaseFormat(self.__connectFour._board[row][column])
+				###
+
+		for line in tmpTab:
+			for letter in line:
+				string += letter
+			string += "#\n"
+		string += "#" * self.__nbColumn * 4 + "#\n"
+		return string
+	
+	def getPlayerId(self, discStyle) -> int:
+		for player in self.__players:
+			if player.discStyle == discStyle:
+				return self.__players.index(player)
+
+	def getCaseFormat(self, discStyle) -> str:
+		if discStyle == '0':
+			return " "
+		playerId = self.getPlayerId(discStyle)
+		return f"{self.__players[playerId].color}{self.__players[playerId].discStyle}{Color_Off}"
+
+	def boardInit(self) -> None:
 		print("Board Init...")
 		self.__connectFour = ConnectFour(self.__nbRow, self.__nbColumn, self.__lineLenToWin)
 		print(self.__connectFour)
 
-	def playerInit(self):
+	def playerInit(self) -> None:
 		print("Players Init...")
 		discStyle = ['#', '%', '&', 'X', 'O'] #TMP
-		names  = ["Rose", "Violette", "Jonquille", "Pétunia", "Tulipe"] #TMP
 		for i in range(self.__nbPlayer):
-			self.__players.append(Player(discStyle[i]))
-			self.__players[i].name = names[i]
+			self.__players.append(Player(discStyle[i % len(discStyle)]))
+			self.__players[i].askName()
+			self.__players[i].askColor()
 			print(self.__players[i])
 
-	def getAllDiscStyle(self):
+	def getAllDiscStyle(self) -> list[str]:
 		return [player.discStyle for player in self.__players]
 
-	def run(self):
+	def run(self) -> None:
 		playerTurn = 0
 
 		while not self.__connectFour.haveDiscWinned(self.getAllDiscStyle()):
 			correctColumnChoose = False
 			while not correctColumnChoose:
 				correctColumnChoose = self.__connectFour.addDisc(self.__players[playerTurn].askColumn(), self.__players[playerTurn].discStyle)
-			print(self.__connectFour)
+			print(self)
 			playerTurn = (playerTurn + 1) % self.__nbPlayer
 
 		print(self.__players[playerTurn - 1].name + " win the game !")
